b08246412f32eba682231df7f1796906c9be77b1
priyansudash1999 <1999priyansudash@gmail.com>
2025-08-17 17:41:50 +0530

    test getInfo of product

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -88,7 +88,21 @@ const removeProduct = async(req, res) => {
 }
 
 const singleProduct = async(req, res) => {
+  try {
+    const {productId} = req.body
+    const product = await productModel.findById(productId)
+    res.json({
+      success: true,
+      product
+    })
 
+  } catch (error) {
+    console.error(error);
+    res.json({
+      success: false,
+      message: error.message
+    })
+  }
 }
 
 export {addProducts, listProducts, removeProduct, singleProduct}
