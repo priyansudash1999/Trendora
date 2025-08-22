d36a4ade282d2dbe244b0d8f1ba9ca67e06f90f0
priyansudash1999 <1999priyansudash@gmail.com>
2025-08-22 22:22:14 +0530

    crack a bug and fix in backend

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,27 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './components/Navbar'
 import Sidebar from './components/Sidebar'
 import { Route, Routes } from 'react-router-dom'
 import Add from "./pages/Add.jsx"
 import List from "./pages/List.jsx"
 import Orders from "./pages/Orders.jsx"
+import Login from './components/Login.jsx'
+import {ToastContainer} from "react-toastify"
+import 'react-toastify/dist/ReactToastify.css'
+
+
+export const backendUrl = import.meta.env.VITE_BACKEND_URL
 
 const App = () => {
+  const [token, setToken] = useState('')
+
+  
+
   return (
     <div className='bg-gray-50 min-h-screen'>
-
-      <Navbar />
-      <hr className='text-gray-300 border-dotted'/>
-      <div className='flex w-full'>
-        <Sidebar />
-        <div className='w-[70%] mx-auto ml-[max(5vw, 25px)] my-8 text-gray-600 text-base '>
-          <Routes>
-            <Route path='/add' element={<Add />}/>
-            <Route path='/list' element={<List />}/>
-            <Route path='/orders' element={<Orders />}/>
-          </Routes>
-        </div>
-      </div>
+      <ToastContainer />
+      {
+        token === '' ? <Login setToken={setToken}/>
+                     : <>
+                        <Navbar />
+                          <hr className='text-gray-300 border-dotted'/>
+                          <div className='flex w-full'>
+                            <Sidebar />
+                            <div className='w-[70%] mx-auto ml-[max(5vw, 25px)] my-8 text-gray-600 text-base '>
+                              <Routes>
+                                <Route path='/add' element={<Add />}/>
+                                <Route path='/list' element={<List />}/>
+                                <Route path='/orders' element={<Orders />}/>
+                              </Routes>
+                            </div>
+                          </div>
+                      </> 
+      }
     </div>
   )
 }
